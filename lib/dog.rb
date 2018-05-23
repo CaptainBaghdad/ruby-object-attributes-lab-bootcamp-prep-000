4197e420c412f78a24f13f37c8189e15151b0aab
Jason Pitts <juniorjr083@gmail.com>
2018-05-23 00:56:08 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,37 @@
+class Dog 
+  
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  
+  def name=(name)
+    @name = name
+    
+  end 
+  
+  
+  def name 
+    @name 
+    
+  end 
+  
+  
+  
+  def breed=(new_breed)
+   @breed = new_breed
+  
+  
+  
+end
+
+
+
+def breed 
+  @breed 
+  
+end 
+
+
+end
