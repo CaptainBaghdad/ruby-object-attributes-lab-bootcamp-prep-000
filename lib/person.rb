285644d15932b5ca8cf9361e7c47e32656c75a6c
Jason Pitts <juniorjr083@gmail.com>
2018-05-23 01:09:07 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,31 @@
+class Person
+  
+  def name 
+    @name
+  end 
+  
+  
+  def name=(name)
+    
+    @name = name 
+    
+  end 
+  
+  def job=(title)
+    
+    @title = title 
+  end 
+  
+  def job 
+    
+    @job 
+    
+  end 
+  
+    
+  
+  
+  
+  
+  
+end 
